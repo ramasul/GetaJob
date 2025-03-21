eb19a8a81ee35f32a5b6d33b52fc86e6f6bdcc38
Rama Sulaiman Nurcahyo <149144632+ramasul@users.noreply.github.com>
2025-03-21 11:37:17 +0700

    Update api.py

--- backend/app/api.py
+++ backend/app/api.py
@@ -116,5 +116,7 @@ async def shutdown_db_client():
 
 # Menjalanakan aplikasi FastAPI
 if __name__ == "__main__":
+    import uvicorn
+    
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.api:app", host="0.0.0.0", port=port)
