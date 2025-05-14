b481244e6e331d2a3ef9f002f081a616c9969771
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2025-05-14 17:20:46 +0700

    feat: Update Cloudinary API in Frontend

--- frontend/src/app/api/cloudinary.js
+++ frontend/src/app/api/cloudinary.js
@@ -62,3 +62,44 @@ export function getImageUrl(id, transformations = {}) {
 
   return `https://res.cloudinary.com/${cloudName}/image/upload/${transformationStr}${publicId}`;
 }
+
+export async function uploadPdf(file, id, user_type, options = {}) {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("id", id);
+    formData.append("user_type", user_type);
+
+    Object.entries(options).forEach(([key, value]) => {
+      formData.append(
+        key,
+        typeof value === "object" ? JSON.stringify(value) : value
+      );
+    });
+
+    const response = await axios.post("/api/cloudinary/pdf/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data.secure_url;
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    throw error;
+  }
+}
+
+export async function deletePdf(id, user_type) {
+  try {
+    const formData = new FormData();
+    formData.append("id", id);
+    formData.append("user_type", user_type);
+
+    const response = await axios.post("/api/cloudinary/pdf/delete", formData);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting from Cloudinary:", error);
+    throw error;
+  }
+}
