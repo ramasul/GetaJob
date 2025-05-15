d5b28a68bb958866203893a0eb8ab4bdea7cd261
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2025-05-15 20:52:44 +0700

    feat: Log Service Integration

--- frontend/src/app/api/logService.js
+++ frontend/src/app/api/logService.js
@@ -1,4 +1,28 @@
 import api from "@auth/api";
 import { cleanEmptyValues } from "@/app/utils/common_fn";
 
-export const logService = {};
+export const logService = {
+  async countJobViews(job_id) {
+    try {
+      const response = await api.get(`/log-views/job/${job_id}/count`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching job views count:", error);
+      throw error;
+    }
+  },
+
+  async addLogs(jobData) {
+    try {
+      const cleanedData = cleanEmptyValues(jobData);
+      const response = await api.post(
+        `/log-views/`,
+        JSON.stringify(cleanedData)
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error adding logs:", error);
+      throw error;
+    }
+  },
+};
